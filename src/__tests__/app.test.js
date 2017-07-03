777975a66d4fd9c157367962c75351e70fb2cf6b
Linas <l.spukas@gmail.com>
2017-07-03 10:40:32 +0300

    Update tests by adding different currency

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -4,22 +4,24 @@ import MockAdapter from 'axios-mock-adapter';
 import app from '../app';
 import api from '../constants';
 
-const { CASH_IN, NATURAL, JURIDICAL } = api;
+const { CASH_IN, NATURAL, JURIDICAL, RATES } = api;
 
 const mock = new MockAdapter(axios);
 
 const cashIn = { percents: 0.03, max: { amount: 5, currency: 'EUR' } };
 const cashOutNatural = { percents: 0.3, week_limit: { amount: 1000, currency: 'EUR' } };
 const cashOutJuridical = { percents: 0.3, min: { amount: 0.5, currency: 'EUR' } };
+const configRates = { EUR: { USD: 1.1497, JPY: 129.53 } };
 
 mock.onGet(CASH_IN).reply(200, cashIn);
 mock.onGet(NATURAL).reply(200, cashOutNatural);
 mock.onGet(JURIDICAL).reply(200, cashOutJuridical);
+mock.onGet(RATES).reply(200, configRates);
 
 describe('app', () => {
   it('works as espected', async () => {
     expect.assertions(1);
     const data = await app('./input.json');
-    await expect(data).toEqual([0.06, 0.9, 87, 3, 0.3, 0.3, 5.0, 0, 0]);
+    await expect(data).toEqual([0.06, 0.9, 0, 0.7, 0.3, 0.3, 5, 0, 0]);
   });
 });
--- src/__tests__/processData.test.js
+++ src/__tests__/processData.test.js
@@ -1,13 +1,16 @@
 import processData from '../processData';
 
 const cashIn = { date: '2016-01-05', user_id: 1, user_type: 'natural', type: 'cash_in', operation: { amount: 200.00, currency: 'EUR' } };
-const natural = { date: '2016-01-06', user_id: 1, user_type: 'natural', type: 'cash_out', operation: { amount: 30000, currency: 'EUR' } };
+const natural = { date: '2016-01-07', user_id: 1, user_type: 'natural', type: 'cash_out', operation: { amount: 1000.00, currency: 'EUR' } };
+const naturalWithCurrency = { date: '2016-01-07', user_id: 1, user_type: 'natural', type: 'cash_out', operation: { amount: 100.00, currency: 'USD' } };
+
 const juridical = { date: '2016-01-06', user_id: 2, user_type: 'juridical', type: 'cash_out', operation: { amount: 300.00, currency: 'EUR' } };
 
 const config = {
   configCashIn: { percents: 0.03, max: { amount: 5, currency: 'EUR' } },
   configNatural: { percents: 0.3, week_limit: { amount: 1000, currency: 'EUR' } },
   configJuridical: { percents: 0.3, min: { amount: 0.5, currency: 'EUR' } },
+  configRates: { EUR: { USD: 1.1497, JPY: 129.53 } },
 };
 
 describe('processData function', () => {
@@ -15,11 +18,15 @@ describe('processData function', () => {
     expect(processData(cashIn, config)).toEqual(0.06);
   });
 
-  it('counts commissions correctyle for natural', () => {
-    expect(processData(natural, config)).toEqual(87);
+  it('counts commissions correctly for natural', () => {
+    expect(processData(natural, config)).toEqual(0);
   });
 
-  it('counts commissions correctyle for juridical', () => {
+  it('counts commissions correctly for natural with different currency', () => {
+    expect(processData(naturalWithCurrency, config)).toEqual(0.3);
+  });
+
+  it('counts commissions correctly for juridical', () => {
     expect(processData(juridical, config)).toEqual(0.9);
   });
 });
